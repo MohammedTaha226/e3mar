c1f5e726d8e4795236f7e6fb133f9bbe49cad98f
Mohammed Taha <mtelrazky@gmail.com>
2023-10-14 20:50:16 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,78 @@
+$(function(){
+	
+    $(window).on("load", function() {
+    	
+		$(".splash").fadeOut();
+		
+        
+        
+    });
+	
+	$(".openMenu").click(function () {
+		
+		$("body").addClass("overflowH");
+		$(".menuMobile").fadeIn();
+		$(".transformPage,.menuMobile .menuContent").addClass("active");
+		
+	});
+	
+	$(".closeX,.BgClose,.menuMobile .menuContent .menuRes li a").click(function () {
+		
+		$("body").removeClass("overflowH");
+		$(".menuMobile").fadeOut();
+		$(".transformPage,.menuMobile .menuContent").removeClass("active");
+		
+	});
+
+
+	$('.the-slider-inner').bxSlider({
+		useCSS: false,
+		auto: true,
+		controls: true,
+		pager: true,
+		autoHover: true,
+		responsive: true,
+		nextSelector: '#slider-next',
+		prevSelector: '#slider-prev',
+		nextText: '<i class="fa fa-angle-right"></i>',
+		prevText: '<i class="fa fa-angle-left"></i>'
+	});
+	
+  	var owlWorks = $('#owlWorks');
+	 
+	  owlWorks.owlCarousel({
+	      
+	      items : 3, //10 items above 1000px browser width
+	      itemsDesktop : [1200,3], //5 items between 1000px and 901px
+	      itemsDesktopSmall : [979,2], // betweem 900px and 601px
+	      itemsTablet: [768,2], //2 items between 600 and 0
+	      itemsMobile : [479,1],// itemsMobile disabled - inherit from itemsTablet option
+	      slideSpeed : 500,
+	      paginationSpeed : 400,
+	      pagination:false,
+	      navigation:true,
+	      autoPlay:true,
+	      navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+	  });
+	  
+  	var owlClients = $('#owlClients');
+	 
+	  owlClients.owlCarousel({
+	      
+	      items : 4, //10 items above 1000px browser width
+	      itemsDesktop : [1200,3], //5 items between 1000px and 901px
+	      itemsDesktopSmall : [979,2], // betweem 900px and 601px
+	      itemsTablet: [768,2], //2 items between 600 and 0
+	      itemsMobile : [479,1],// itemsMobile disabled - inherit from itemsTablet option
+	      slideSpeed : 500,
+	      paginationSpeed : 400,
+	      pagination:false,
+	      navigation:true,
+	      autoPlay:true,
+	      navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"]
+	  });
+	  
+	
+	
+	
+});
